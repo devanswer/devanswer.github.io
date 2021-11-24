4cd080fdb34c124237b3db619da5e2cd3f3d1ca4
gomnam1336 <gomnam1336@yahoo.com>
2021-11-24 11:02:23 +0330

    show project template

---
+++ templates/one-page/show-project/js/script.js
@@ -0,0 +1,15 @@
+let toggleMenu = document.querySelectorAll(".navbar .toggle-menu");
+let navLinks = document.querySelector(".nav-links");
+let links = document.querySelectorAll(".nav-links a");
+function toggleLinks(ourArray, ourFubction) {
+  ourArray.forEach((element) => {
+    element.addEventListener("click", () => {
+      ourFubction();
+    });
+  });
+}
+function toggleNavLinks() {
+  navLinks.classList.toggle("active");
+}
+toggleLinks(toggleMenu, toggleNavLinks);
+toggleLinks(links, toggleNavLinks);
